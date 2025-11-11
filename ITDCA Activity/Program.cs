9fb44fc3e99ddddb88047fa96cf996a9d726d363
Sxoony <zamirkruger92@gmail.com>
2025-11-11 13:30:57 +0200

    Add project files.

---
+++ ITDCA Activity/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ITDCA_Activity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var ingredients = new List<string>
+        {
+            "eggs","flour","sugar","milk","bread"
+        };
+
+
+            HashSet<string> recipe = new HashSet<string>(ingredients);
+
+            HashSet<string> sandwich = new HashSet<string> { "eggs", "bread" };
+            Console.WriteLine("Micaela's sandwich list: eggs, bread\n");
+
+            Console.WriteLine("Micaela's full pantry list: ");
+            foreach (var  ingredient in ingredients)
+            {
+                Console.Write(ingredient+" ");
+            }
+            Console.WriteLine("\n");
+
+            var itemsexcluded = new HashSet<string>(recipe);
+            itemsexcluded.ExceptWith(sandwich);
+            foreach (var except in itemsexcluded)
+            {
+                Console.WriteLine(except);
+            }
+        }
+    }
+}
